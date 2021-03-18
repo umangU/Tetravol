cae82ac181a33b5097d86bb144d99bd7dcc27020
umangU <72771903+umangU@users.noreply.github.com>
2021-03-18 19:37:36 +1100

    Code Files Integrated

---
+++ tetravol.m
@@ -0,0 +1,5 @@
+%Function to calculate the volume of the tetrahedron from the side length of tetrahedron
+function V = tetravol (g)
+%The volume of tetrahedron is stored in the variable V after the calculation
+V = (sqrt(2)/12)*(g^3); %sqrt to calculate the square root of the number
+end
---
+++ tetravol_program.m
@@ -0,0 +1,3 @@
+g = input("Enter the side length of tetrahedron: ");
+V = tetravol(g); %tetravol function called while supplying the user inputs as arguments
+fprintf('If the side length of the tetrahedron is %d,\nthen the Volume of the tetrahedron will be: %d \n',g,V);
