0e6df063cdf76c4f7d25f30cbc7fb5ffe0dd4fbb
umangU <72771903+umangU@users.noreply.github.com>
2021-05-05 22:55:03 +1000

    Code Updated

--- tetravol_program.m
+++ tetravol_program.m
@@ -1,3 +1,4 @@
 g = input("Enter the side length of tetrahedron: ");
-V = tetravol(g); %tetravol function called while supplying the user inputs as arguments
+% tetravol function called while supplying the user inputs as arguments
+V = tetravol(g); 
 fprintf('If the side length of the tetrahedron is %d,\nthen the Volume of the tetrahedron will be: %d \n',g,V);
